d2ed6dc6ebd5bc401e91340a7bff2dff8112b424
crystallite-hobby <crystallite.hobby@gmail.com>
2025-09-09 20:15:00 +0300

    Layouts added

--- src/lines-layout.rs
+++
@@ -1,6 +0,0 @@
-// Re-export contents of lines_layout.rs for the custom test target.
-// This allows running the same tests via an integration test specified in
-// Cargo.toml without duplicating code.
-
-include!("lines_layout.rs");
-
